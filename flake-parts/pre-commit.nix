fd87066ee36c5084819583e4d73bd50fae0850c3
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-03-20 18:52:56 -0500

    fix: sqlx pre-commit-hook
    The hook was failing when running `nix flake check` due to the flake
    sandbox. It turns out that setting `enable = false` on the hook causes
    pre-commit-hooks.nix to skip that check during sandbox builds. This
    means that the hook is able to catch issues in code before it is
    committed, but does not cause issues for CI.

--- flake-parts/pre-commit.nix
+++ flake-parts/pre-commit.nix
@@ -3,7 +3,12 @@
   self,
   ...
 }: {
-  perSystem = {...}: {
+  perSystem = {
+    lib,
+    pkgs,
+    self',
+    ...
+  }: {
     pre-commit = {
       check.enable = true;
 
@@ -14,9 +19,20 @@
           rustfmt.enable = true;
 
           # sqlx check custom hook
-          sql-prepare = {
-            enable = true;
-            entry = "cargo sqlx prepare --merged";
+          sql-prepare = let
+            wrapper = pkgs.symlinkJoin {
+              name = "sqlx-prepare-wrapper";
+              paths = [self'.packages.rust-toolchain pkgs.sqlx-cli];
+              nativeBuildInputs = [pkgs.makeWrapper];
+              postBuild = ''
+                wrapProgram $out/bin/cargo-sqlx \
+                  --prefix PATH: ${pkgs.lib.makeBinPath [self'.packages.rust-toolchain]}
+                  --set SQLX_OFFLINE true
+              '';
+            };
+          in {
+            enable = false; # only run in the devShell
+            entry = "${wrapper}/bin/cargo-sqlx prepare --merged";
             # add `--check` to check only. Without it the file will be updated when the hook is run
             # entry = "cargo sqlx prepare --merged --check";
             pass_filenames = false;
