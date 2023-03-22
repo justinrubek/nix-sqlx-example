54e608ec1b96c5b842e0c208a32acdd198213b00
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-03-21 19:31:02 -0500

    fix: properly enable pre-commit-checks
    Previously, b072196a67407f95bd7802040a23a01f65c58cff was incorrect.
    The `enable` option of an individual hook disables it entirely.
    The flake-parts module has a top level option, `check.enable` that will
    disable ALL hooks from running, except on devShells.
    
    This is sufficient for now. Ideally the individual hook could be
    specified, but if devShells are used properly then commits are blocked.
    Some care will be needed to make sure that nobody fails to do so.

--- flake-parts/pre-commit.nix
+++ flake-parts/pre-commit.nix
@@ -4,13 +4,12 @@
   ...
 }: {
   perSystem = {
-    lib,
     pkgs,
     self',
     ...
   }: {
     pre-commit = {
-      check.enable = true;
+      check.enable = false; # only run on pre-commit, not in CI
 
       settings = {
         src = ../.;
@@ -19,20 +18,9 @@
           rustfmt.enable = true;
 
           # sqlx check custom hook
-          sql-prepare = let
-            wrapper = pkgs.symlinkJoin {
-              name = "sqlx-prepare-wrapper";
-              paths = [self'.packages.rust-toolchain pkgs.sqlx-cli];
-              nativeBuildInputs = [pkgs.makeWrapper];
-              postBuild = ''
-                wrapProgram $out/bin/cargo-sqlx \
-                  --prefix PATH: ${pkgs.lib.makeBinPath [self'.packages.rust-toolchain]}
-                  --set SQLX_OFFLINE true
-              '';
-            };
-          in {
-            enable = false; # only run in the devShell
-            entry = "${wrapper}/bin/cargo-sqlx prepare --merged";
+          sql-prepare = {
+            enable = true;
+            entry = "cargo sqlx prepare --merged";
             # add `--check` to check only. Without it the file will be updated when the hook is run
             # entry = "cargo sqlx prepare --merged --check";
             pass_filenames = false;
